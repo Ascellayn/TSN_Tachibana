5e7a048f2239fb280f7e07021477b8e940cc775b
Ascellayn <root.siriusb@gmail.com>
2026-01-23 16:36:06 +0100

    F¤Type | Fondation for Tachibana.json

---
+++ Tachibana/Type.py
@@ -0,0 +1,64 @@
+"""
+### Tachibana.json
+```
+{
+	"_Version": "{App.Version}",
+	"Config": {},
+	"Servers": {
+		"SSH": {
+			"{Address}:{Port}": {
+				"Name": "{User Specified Server Name}",
+				"Profiles": {
+					"{User Specified Profile Name}": {
+						"User": "{Connection Username}",
+						"Password": "{Password}" | null,
+						"Passkey": " {Path to SSH Key}" | null,
+						"Folder_Remote": "{Remote Path to mount Locally}",
+						"Folder_Local": "{Local Path to mount Remote to}"
+					}
+				}
+			}
+		}
+	}
+}
+```
+"""
+from Globals import Any, TypedDict;
+import enum;
+
+
+
+
+
+# Basic Types
+type Profile = dict[str, Connection_Info];
+class Server_Type(enum.Enum):
+	SSH = "SSH";
+
+
+
+class Server(TypedDict):
+	Name: str; Profiles: Profile;
+
+
+class Connection_Info(TypedDict):
+	User: str; Password: str | None;
+
+
+
+
+
+# Specific Types
+class SSH(Connection_Info):
+	Passkey: str | None;
+	Folder_Remote: str; Folder_Local: str;
+
+
+
+
+
+class Tachibana_Entries(TypedDict):
+	"""Tachibana.json File Format"""
+	_Version: str;
+	Config: dict[str, Any];
+	Servers: dict[Server_Type, Server];
