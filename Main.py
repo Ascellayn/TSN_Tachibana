d44a6b8890dc185dfd0ef3410fb3cfee421d565e
Ascellayn <ascellayn.sagittarii+github@sirio-network.com>
2026-02-02 12:24:01 +0100

    FR¤Main | Auto Backup on Newer Version - Better Corruption Check - Correct Typing

--- Main.py
+++ Main.py
@@ -2,18 +2,27 @@ from Tachibana import *;
 
 def Ignition() -> None:
 	Log.Stateless("Loading Tachibana JSON...");
-	Tachibana: Type.Tachibana_JSON = File.JSON_Read("Tachibana.json"); # pyright: ignore[reportAssignmentType] | In normal circumstances, this SHOULD return a Tachibana_JSON
+	Tachibana: Type.Tachibana_JSON = cast(Type.Tachibana_JSON, File.JSON_Read("Tachibana.json"));
 
 	# File Checks
 	if (not Misc.All_Includes(Tachibana.keys(), ["_Version", "Config", "Servers"])):
 		Data.Recreate();
 
 
-	if (not type(Tachibana["_Version"]) is tuple): Data.Recreate();
-	if (not type(Tachibana["Config"]) is dict): Data.Recreate();
-	if (not type(Tachibana["Servers"]) is dict): Data.Recreate();
+	if (not type(Tachibana["_Version"]) is list): Data.Recreate();
+	elif (not type(Tachibana["Config"]) is dict): Data.Recreate();
+	elif (not type(Tachibana["Servers"]) is dict): Data.Recreate();
 
-	Misc.Under_At(App.Version, Tachibana["_Version"]);
+	if (Misc.Under_At(Tachibana["_Version"], App.Version) == 0):
+		# Upgrading version json logic here
+		Log.Warning(f"{App.Name} was updated but the saved data is too old to function with this version! Attempting to upgrade...");
+		File.JSON_Write(f"Tachibana-v{''.join(String.ify_Array(Tachibana['_Version']))}.bak", Tachibana);
+		...
+
+		File.JSON_Write("Tachibana.json", Tachibana);
+
+	Log.Awaited().OK();
+
 
 
 if (__name__ != "__main__"): TSN_Abstracter.Import_Unsupported();
