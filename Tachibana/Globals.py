5a795d0b4e609e3e715d6de1e6ba1a9726f31db7
Ascellayn <ascellayn.sagittarii+github@sirio-network.com>
2026-02-02 12:20:18 +0100

    B¤Globals | Correct Typing

--- Tachibana/Globals.py
+++ Tachibana/Globals.py
@@ -1,7 +1,7 @@
 from TSN_Abstracter import *;
 from . import Type;
 
-Tachibana: Type.Tachibana_JSON = File.JSON_Read("Tachibana.json"); # pyright: ignore[reportAssignmentType] | In normal circumstances, this SHOULD return a Tachibana_JSON
+Tachibana: Type.Tachibana_JSON;
 
 class Data:
 	""" This class would have been inside its own module, but the IDE gets angry because it's stupid and doesn't realize Tachibana is declared in this very file even if I explicitly import it """
@@ -9,7 +9,7 @@ class Data:
 	def Save() -> None:
 		global Tachibana;
 		Log.Stateless(f"Saving Tachibana.json...");
-		Tachibana["_Version"] = App.Version;
+		Tachibana["_Version"] = list(App.Version);
 		File.JSON_Write("Tachibana.json", Tachibana);
 		Log.Awaited().OK();
 
@@ -19,7 +19,8 @@ class Data:
 		Log.Error("Tachibana.json is corrupted or missing, creating a backup if it exists and recreating the file!");
 		if (File.Exists("Tachibana.json")): File.JSON_Write(f"Tachibana-{Time.Get_Unix()}.bak", Tachibana);
 		Tachibana = {
-			"_Version": App.Version,
+			"_Version": list(App.Version),
 			"Config": {},
 			"Servers": {}
 		};
+		Data.Save();
