2536070106a1cd056adf49603af0deb9fca35b0e
Ascellayn <ascellayn.sagittarii+github@sirio-network.com>
2026-02-08 03:08:24 +0100

    F¤Main | Early TUI Interface, Version Bump

--- Main.py
+++ Main.py
@@ -1,5 +1,86 @@
 from Tachibana import *;
 
+
+
+
+
+
+
+
+
+
+def Quit(Entries: TUI.Menu.Entries | None) -> None:
+	TUI.Exit(); exit(0);
+
+
+
+
+def Menu_SSH_Create(*args: Any) -> None:
+	Entries: TUI.Menu.Entries = [
+		TUI.Menu.Entry(20, "Create SSH Connection"),
+		TUI.Menu.Entry(11, "Server Name", "Specify a friendly name for you to remember this SSH Server."),
+		TUI.Menu.Entry(11, "Server Address", "Specify the IP Address or Hostname the SSH Server is on."),
+		TUI.Menu.Entry(11, "Server Port", "Specify which port the SSH Server is on.", Value="22"),
+		TUI.Menu.Entry(11, "Username", "The username we should log on as.", Value="root"),
+		TUI.Menu.Entry(11, "Password", "The password we should use to login."),
+		TUI.Menu.Entry(11, "SSH Key", "An absolute path to an SSH Key we should use.", Value="~/.ssh/id_rsa"),
+		TUI.Menu.Entry(20, ""),
+		TUI.Menu.Entry(20, "SFTP Settings", Indentation=1),
+		TUI.Menu.Entry(11, "Remote Folder", "Specify the folder you'd like to access from the Server remotely.", Value="/", Indentation=1),
+		TUI.Menu.Entry(11, "Local Folder", "Specify a place in your filesystem where you would like to mount the remote folder.", Value="/media/SFTP", Indentation=1),
+		TUI.Menu.Entry(20, ""),
+		TUI.Menu.Entry(20, "Workarounds"),
+		TUI.Menu.Entry(10, "Spoof Terminal", "Enable to spoof to the Server which Terminal you are using. Useful if the Server does not support your Terminal."),
+			TUI.Menu.Entry(11, "Exported Terminal", "Increases compatibility if your Server does not support your Terminal.", 1, Value="xterm-256color"),
+		TUI.Menu.Entry(20, ""),
+		TUI.Menu.Entry(1, f"Save Server", "[TBD - UNAVAILABLE] Create a brand new entry with the provided information.", Unavailable=True, Function=Menu_Main),
+		TUI.Menu.Entry(1, f"Cancel", "Return to the Menu with all your saved SSH Servers.ù", Function=Menu_SSH),
+		TUI.Menu.Entry(20, ""),
+		TUI.Menu.Entry(1, f"Return to Main Menu", Function=Menu_Main)
+	];
+	TUI.Menu.Interactive(Entries);
+	Menu_Main();
+
+
+def Menu_SSH(*args: Any) -> None:
+	Entries: TUI.Menu.Entries = [
+		TUI.Menu.Entry(20, "Secure Shell Server - Connections"),
+		TUI.Menu.Entry(20, "TO BE DONE - DISPLAY SERVERS PER NAME / ADDRESS"),
+		TUI.Menu.Entry(20, ""),
+		TUI.Menu.Entry(1, f"Register New Server", "Register a brand new SSH Server.", Function=Menu_SSH_Create),
+		TUI.Menu.Entry(1, f"Return to Main Menu", Function=Menu_Main)
+	];
+	TUI.Menu.Interactive(Entries);
+	Quit(None);
+
+
+
+
+
+def Menu_Main(*args: Any) -> None:
+	Entries: TUI.Menu.Entries = [
+		TUI.Menu.Entry(20, "Quick Actions"),
+		TUI.Menu.Entry(0, "Create new SSH Connection", Function=Menu_SSH_Create),
+		TUI.Menu.Entry(20, ""),
+		TUI.Menu.Entry(20, "Server Protocols"),
+		TUI.Menu.Entry(0, "Secure Shell Server", Function=Menu_SSH),
+		TUI.Menu.Entry(20, ""),
+		TUI.Menu.Entry(20, ""),
+		TUI.Menu.Entry(1, f"Exit {App.Name}", "You may also press \"ESC\" to quit Tachibana at any point.", Function=Quit)
+	];
+	TUI.Menu.Interactive(Entries);
+	Quit(None);
+
+
+
+
+
+
+
+
+
+
+
 def Ignition() -> None:
 	Log.Stateless("Loading Tachibana JSON...");
 	Tachibana: Type.Tachibana_JSON = cast(Type.Tachibana_JSON, File.JSON_Read("Tachibana.json"));
@@ -23,6 +104,8 @@ def Ignition() -> None:
 
 	Log.Awaited().OK();
 
+	TUI.Init();
+	Menu_Main();
 
 
 if (__name__ != "__main__"): TSN_Abstracter.Import_Unsupported();
