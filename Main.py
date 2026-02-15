5232a2f1e01209996f2d7be171e9ceacb16ab6cc
Ascellayn <ascellayn.sagittarii+github@sirio-network.com>
2026-02-15 15:54:00 +0100

    B¤Main | Avoid crash for Wireguard due to missing Profiles support

--- Main.py
+++ Main.py
@@ -61,11 +61,15 @@ def Menu_Settings() -> None:
 
 
 def Keybind_Delete_Server(Entry: TUI.Menu.Entry) -> bool:
+	Description: str = f"Are you sure you want to delete {Entry.ID}?\n"
+
+	if ("Profiles" in Tachibana["Servers"][Entry.Arguments[0]][Entry.Arguments[1]].keys()): # pyright: ignore[reportArgumentType]
+		Description += f"This action will delete everything under \"{Entry.Arguments[1]}\" which, will in turn delete {len(Tachibana['Servers'][Entry.Arguments[0]][Entry.Arguments[1]]['Profiles'])} profiles.";
+	else: Description += f"This action will delete internally \"{Entry.Arguments[1]}\"";
+	
 	if (Entry.ID and "Yes" == TUI.Menu.Popup(
 		f"Confirm Deleting Server Entry",
-		f"\
-Are you sure you want to delete {Entry.ID}?\n\
-This action will delete everything under \"{Entry.Arguments[1]}\" which, will in turn delete {len(Tachibana['Servers'][Entry.Arguments[0]][Entry.Arguments[1]]['Profiles'])} profiles.",
+		Description,
 		TUI.Menu.Entry(12, Arguments=["Yes", "No"], Value="No")
 	)):
 		del Tachibana['Servers'][Entry.Arguments[0]][Entry.Arguments[1]];
